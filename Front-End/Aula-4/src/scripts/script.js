dfac8cfcc8f0bc757debe17b5e2af6fbfc61b0d6
avasconcelos-tech <a.vasconcelos@edu.senai.br>
2026-02-20 15:57:13 -0300

    .

---
+++ Front-End/Aula-4/src/scripts/script.js
@@ -0,0 +1,18 @@
+const tempoAgora = new Date()
+
+const hora = tempoAgora.getHours()
+
+const dataFormatada = tempoAgora.toLocaleDateString('pt-BR')
+const horaFormatada = tempoAgora.toLocaleTimeString('pt-BR')
+
+console.log("Hoje é dia: " + dataFormatada)
+console.log("Horário: " + horaFormatada)
+
+console.log("Só a hora: " + hora)
+
+if(hora > 12 && hora < 18){
+alert("Boa tarde!")
+} else {
+    (hora >= 19)
+    alert("Boa noite!")
+}

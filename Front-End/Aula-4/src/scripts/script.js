dc647211e8f2dd89f4fa0cedecea1ab27fdece1f
avasconcelos-tech <a.vasconcelos@edu.senai.br>
2026-02-25 12:48:58 -0300

    atualizacoes front-end

--- Front-End/Aula-4/src/scripts/script.js
+++ Front-End/Aula-4/src/scripts/script.js
@@ -10,9 +10,9 @@ console.log("Horário: " + horaFormatada)
 
 console.log("Só a hora: " + hora)
 
-if(hora > 12 && hora < 18){
+
+if(hora >= 12 && hora < 18){
 alert("Boa tarde!")
-} else {
-    (hora >= 19)
+} else if (hora >= 18 && hora < 6 ) { 
     alert("Boa noite!")
 }

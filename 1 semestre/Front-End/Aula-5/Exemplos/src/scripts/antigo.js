ed8e74eb58f6b7c86383c4caeceea5746a7aa897
avasconcelos-tech <a.vasconcelos@edu.senai.br>
2026-03-04 15:56:51 -0300

    conclusao aula-5

--- 1 semestre/Front-End/Aula-5/Exemplos/src/scripts/antigo.js
+++ 1 semestre/Front-End/Aula-5/Exemplos/src/scripts/antigo.js
@@ -13,22 +13,3 @@ console.log("5. A cor do cabeçalho:", topo.style.backgroundColor);
 const estilosReais = window.getComputedStyle(topo);
 console.log("5. A cor real do cabeçalho (do CSS):", estilosReais.backgroundColor);
 
-
-console.log("=== EXPLORANDO PROPRIEDADES VISUAIS ===");
-
-console.log("Texto do cabeçalho:", topo.innerText); 
-console.log("HTML interno do cabeçalho:", topo.innerHTML);
-
-const fotoLasanha = listaCards[0].getElementsByTagName("img")[0];
-console.log("URL da imagem do card:", fotoLasanha.src);
-
-console.log("Largura real do topo na tela:", topo.offsetWidth + "px");
-
-const checkbox = document.getElementById("bt_menu");
-console.log("O menu lateral está ativado?:", checkbox.checked);
-
-console.log("Classes do card:", listaCards[0].classList);
-
-console.log("LOG do elemento (visão HTML):", topo);
-
-console.dir(topo);
--- 1 semestre/Front-End/Aula-5/Exemplos/src/scripts/novo.js
+++ 1 semestre/Front-End/Aula-5/Exemplos/src/scripts/novo.js
@@ -2,10 +2,36 @@ const tituloNhoque = document.querySelector("#card-nhoque h3");
 
 const botoesCompra = document.querySelectorAll(".btn-pedido");
 
-const terceiroCard = document.querySelector(".card:nth-child (4)");
+const terceiroCard = document.querySelector(".card:nth-child(3)");
+
+const nomeCompleto = document.querySelector('#nome')
 
 console.log("1. Mostrando o Título NHOQUE (pelo ID)", tituloNhoque);
 
 console.log("2. Quantidade de botões de pedido: ", botoesCompra.length);
 
-console.log(terceiroCard);
+console.log(" 3. A terceira posição da class .card: ", terceiroCard);
+
+const imgLasanha = document.querySelector('img[alt="Lasanah Tech"]')
+
+if(tituloNhoque){
+    console.log("Título CAPTURADO: ", tituloNhoque.innerText)
+}
+
+const saudacao = document.querySelector('#boas-vindas')
+
+const agora = new Date()
+const hora = agora.getHours()
+
+saudacao.textContent = hora < 18 ? "Bem vindo, bom almoço" : "Bem vindo, boa janta"
+
+nomeCompleto.innerHTML = "<strong>Ana Katy</strong> <em>Romão</em> Vasconcellos" 
+
+
+const foto = document.querySelector('#foto-destaque')
+
+const caminhoAtual = foto.getAttribute('src')
+
+foto.setAttribute('src', 'src/images/esgotado.jpg')
+foto.setAttribute('alt', 'Prato esgotado!!')
+

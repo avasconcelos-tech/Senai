265df72a225707355ed0b5da2e4c9818b262cfb9
avasconcelos-tech <a.vasconcelos@edu.senai.br>
2026-03-04 14:06:31 -0300

    .front-end

--- 1 semestre/Front-End/Aula-5/Exemplos/src/scripts/novo.js
+++ 1 semestre/Front-End/Aula-5/Exemplos/src/scripts/novo.js
@@ -1,26 +1,11 @@
-const tituloNhoque = document.querySelector('#card-nhoque h3');
+const tituloNhoque = document.querySelector("#card-nhoque h3");
 
-const botoesCompra = document.querySelectorAll('.btn-pedido');
+const botoesCompra = document.querySelectorAll(".btn-pedido");
 
-const imgLasanha = document.querySelector('img[alt="Lasanha Tech"]');
+const terceiroCard = document.querySelector(".card:nth-child (4)");
 
-const checkMenu = document.querySelector('#bt_menu');
+console.log("1. Mostrando o Título NHOQUE (pelo ID)", tituloNhoque);
 
-console.log("=== TESTES COM QUERY SELECTOR ===");
+console.log("2. Quantidade de botões de pedido: ", botoesCompra.length);
 
-if (tituloNhoque) {
-    console.log("Título capturado:", tituloNhoque.innerText);
-}
-
-console.log("Quantidade de botões de pedido:", botoesCompra.length);
-
-if (botoesCompra.length > 0) {
-    botoesCompra[0].textContent = "Comprar Agora!";
-    console.log("Texto do primeiro botão alterado com sucesso.");
-}
-
-const precoLasanha = document.querySelector('.card .preco');
-console.log("Preço da Lasanha:", precoLasanha.innerText);
-
-const elementoInexistente = document.querySelector('.classe-que-nao-existe');
-console.log("Resultado de seletor inexistente:", elementoInexistente); // Retorna null
+console.log(terceiroCard);

fe8fce2340d6191ab845265cb534de91e2461a46
avasconcelos-tech <a.vasconcelos@edu.senai.br>
2026-07-31 15:18:44 -0300

    continuação da revisão

---
+++ 4 semestre/Front-End 2/aula 0 - revisão/exemplos/techfood/script.js
@@ -0,0 +1,70 @@
+class Prato {
+  constructor(nome, preco, categoria) {
+    this.nome = nome;
+    this.preco = preco;
+    this.categoria = categoria;
+  }
+
+  formatarPreco() {
+    return `R$ ${this.preco.toFixed(2).replace(".", ",")}`;
+  }
+
+  aplicarDesconto(percentual) {
+    this.preco = this.preco * (1 - percentual / 100);
+  }
+}// fim da classe
+
+
+const cardapio = [
+    new Prato("Feijoada Completa", 42.90, "Prato Principal" ),
+    new Prato("Moqueca de Peixe", 58.00, "Prato Principal"),
+    new Prato("Coxinha Artesanal", 8.50, "Petisco"),
+    new Prato("Brigadeiro Gourmet", 6.00, "Sobremesa"),
+    new Prato("Morango do amor (Pistache)", 15.00, "Sobremesa"),
+      new Prato("Suca de Maracujá", 12.00, "Bebida"),
+]
+
+console.log("=== Pratos Criados ===")
+cardapio.forEach(p =>{
+    console.log(`${p.nome} ➡️ ${p.formatarPreco()}`)
+})
+
+const containerCardapio = document.querySelector("#cardapio")
+
+function criarCardPrato(prato){
+    const card = document.createElement('div')
+    card.className = 'card'
+
+    card.innerHTML = 
+    `
+    <h3>${prato.nome}</h3>
+    <span class="categoria">${prato.categoria}</span>
+    <div class="preco">${prato.formatarPreco()}</div>
+    `
+
+
+    card.addEventListener('click', () =>{
+        alert(
+            `
+            🍽️${prato.nome} \n\n
+            Categoria: ${prato.categoria} \n
+            Preço: ${prato.formatarPreco()}
+            `
+        )
+    })
+
+    return card
+}//fim da função criar Card Prato
+
+function renderizarCardapio(){
+    containerCardapio.innerHTML = ''
+
+cardapio.forEach(prato =>{
+    const card = criarCardPrato(prato)
+
+    containerCardapio.appendChild(card)
+})
+}//fim função renderizarCardapio
+
+renderizarCardapio()
+

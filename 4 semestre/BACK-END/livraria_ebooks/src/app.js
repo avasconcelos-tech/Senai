330ad61d4d34b2022cdee3e1bc9d89451771e41e
avasconcelos-tech <a.vasconcelos@edu.senai.br>
2026-09-23 07:36:36 -0300

    criação do arquivo swagger.js e swagger_output.json e correção do arquivo app.js

--- 4 semestre/BACK-END/livraria_ebooks/src/app.js
+++ 4 semestre/BACK-END/livraria_ebooks/src/app.js
@@ -1,12 +1,20 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger_output.json");
+
 const app = express();
-const routes = require('./routes');
+const routes = require("./routes");
 
 app.use(cors());
 app.use(express.json());
+app.use("/public", express.static(path.join(__dirname, "..", "public")));
 
 // Registro de rotas
 app.use(routes);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 module.exports = app;
